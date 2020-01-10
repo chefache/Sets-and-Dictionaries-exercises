91d077c4122ac68e4ae97b0214470bf5a91332b1
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-10 23:42:49 +0200

    Add files via upload

---
+++ Sets and Dictionaries/Count same values in array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_same_values_in_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()
+                 .Split()
+                 .Select(double.Parse)
+                 .ToArray();
+
+            var dict = new Dictionary<double, int>();
+
+            foreach (var num in inputNumbers)
+            {
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 0);
+                }
+                dict[num]++;
+            }
+            foreach (var num in dict)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+        }
+    }
+}
