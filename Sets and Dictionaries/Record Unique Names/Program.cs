65f4a808b028c52db9e6491de5e541bb0ebe9711
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-14 22:51:02 +0200

    Add files via upload

---
+++ Sets and Dictionaries/Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNamesCount = int.Parse(Console.ReadLine());
+            var hashset = new HashSet<string>();
+            for (int i = 0; i < inputNamesCount; i++)
+            {
+                var name = Console.ReadLine();
+                hashset.Add(name);
+            }
+            foreach (var name in hashset)
+            {
+                Console.WriteLine(string.Join(" ", name));
+            }
+        }
+    }
+}
