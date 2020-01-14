311f9104ef94d1bf8c89a0c08a0f9241e58d7d6a
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-14 23:46:50 +0200

    Add files via upload

---
+++ Sets and Dictionaries/SoftUni Party/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guests = new HashSet<string>();          
+            while (true)
+            {
+                var input = Console.ReadLine();
+                guests.Add(input);
+                if (input == "PARTY")
+                {
+                    guests.Remove("PARTY");
+                    while (input != "END")
+                    {
+                        input = Console.ReadLine();
+                        guests.Remove(input);
+                    }
+                    if (input == "END")
+                    {                   
+                        break;
+                    }
+                }             
+            }
+            Console.WriteLine(guests.Count);
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(string.Join(" ", guest));
+            }
+        }
+    }
+}
