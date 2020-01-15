2aabd7dcd0232608f95fb16f58184954e0078af9
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-15 17:32:37 +0200

    Add files via upload

---
+++ Sets and Dictionaries/Count Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var @string = Console.ReadLine().ToCharArray();
+            var charDict = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < @string.Length; i++)
+            {
+                if (!charDict.ContainsKey(@string[i]))
+                {
+                    charDict.Add(@string[i], 0);
+                }
+                charDict[@string[i]]++;
+            }
+            foreach (var @char in charDict)
+            {
+                Console.WriteLine($"{@char.Key}: {@char.Value} time/s");
+            }
+        }
+    }
+}
