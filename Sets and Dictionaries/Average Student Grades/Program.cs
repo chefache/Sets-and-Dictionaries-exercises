eb340fc4cf21e736187219faec42ef7103c8b6dd
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-15 17:29:10 +0200

    Add files via upload

--- Sets and Dictionaries/Average Student Grades/Program.cs
+++ Sets and Dictionaries/Average Student Grades/Program.cs
@@ -8,26 +8,27 @@ namespace Average_Student_Grades
     {
         static void Main(string[] args)
         {
-            var studentInfo = new Dictionary<string, List<double>>();
+            int numInputs = int.Parse(Console.ReadLine());
 
-            int numNames = int.Parse(Console.ReadLine());
+            var studentRecords = new Dictionary<string, List<double>> ();
 
-            for (int i = 0; i < numNames; i++)
+            for (int i = 0; i < numInputs; i++)
             {
-                var nameAndGrade = Console.ReadLine()
+                var studentInfo = Console.ReadLine()
                     .Split();
-                string name = nameAndGrade[0];
-                double grade = double.Parse(nameAndGrade[1]);
-                if (!studentInfo.ContainsKey(name))
+                var studentName = studentInfo[0];
+                var studentGrade = double.Parse(studentInfo[1]);
+
+                if (!studentRecords.ContainsKey(studentName))
                 {
-                    studentInfo.Add(name, new List<double>());
+                    studentRecords[studentName] = new List<double>();
                 }
-                studentInfo[name].Add(grade);
+                studentRecords[studentName].Add(studentGrade);
             }
 
-            foreach (var student in studentInfo)
+            foreach (var student in studentRecords)
             {
-                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value.Select(x => x.ToString("F2"))):F2} (avg:{student.Value.Average():F2})");
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value.Select(x => x.ToString("F2")))} (avg: {student.Value.Average():F2})");
             }
         }
     }
