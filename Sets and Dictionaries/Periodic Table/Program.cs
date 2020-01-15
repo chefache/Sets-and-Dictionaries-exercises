f2a2feebd1ddac095169d7d3f273ddf7c3eb7abf
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-15 17:33:25 +0200

    Add files via upload

---
+++ Sets and Dictionaries/Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var elementsSet = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var chemicalElements = Console.ReadLine()
+                    .Split(" ");
+
+                foreach (var element in chemicalElements)
+                {
+                    elementsSet.Add(element);
+                }
+            }
+            Console.WriteLine(string.Join(" ", elementsSet));
+        }
+    }
+}
