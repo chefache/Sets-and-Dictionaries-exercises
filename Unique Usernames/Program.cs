3c614d6ec6fbc65a7ee59e69f27370a3cf4e17c7
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-15 17:33:55 +0200

    Add files via upload

---
+++ Unique Usernames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+          //  var sortedSet = set.OrderBy(x => x.Length);
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
