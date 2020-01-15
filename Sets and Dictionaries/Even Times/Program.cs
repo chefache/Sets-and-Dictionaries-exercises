ebedd9dfc63f7d7c0c812f17170c878b19221076
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-15 17:33:01 +0200

    Add files via upload

---
+++ Sets and Dictionaries/Even Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integersCount = int.Parse(Console.ReadLine());
+
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < integersCount; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 0);
+                }
+                dict[num]++;
+            }
+
+            foreach (var num in dict)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                }
+            }
+        }
+    }
+}
